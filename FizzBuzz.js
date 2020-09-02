6a6f1f70fc82a3815b06900884f35bc05cc8ac77
Rahul Sharma <rahulsharma94595@gmail.com>
2020-09-02 19:27:09 +0530

    FizzBuzz Algorithm

---
+++ FizzBuzz.js
@@ -0,0 +1,10 @@
+/* FizzBuzz algorithm print Fizz if it is divided by 3, Buzz if it is divided by 5, if it is divisible by both 3 and 5 it prints FizzBuzz*/
+function FizzBuzz(num) {
+    for(let i=1; i<= num; i++) {
+        if(i%15 === 0) console.log('FizzBuzz');
+        else if(i%3 === 0) console.log('Fizz');
+        else if(i%5 === 0 )console.log('FizzBUzz');
+        else console.log(i);
+    }
+}
+console.log(FizzBuzz(30));
--- binary-search.js
+++ binary-search.js
@@ -1,3 +1,7 @@
+/* Binary search algorithm is a searching algorithm that helps you to find the desired value in array.It always worked in sorted numbers.
+   This algorithm takes the Beginning and the End Index of the array to find the midPoint of the array.It works same as Divide and conquer rule.
+   To find midIndex of the array we use formula:-- (begin + end)/2;
+*/
 let arr = [1,2,3,4,12,9,8,6];
 arr.sort(function(a , b) {return a-b;});
 function binarySearch(array, targetValue) {
