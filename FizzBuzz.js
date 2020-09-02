3a663f535593a7baab284f019c5e46685e46eae0
Rahul Sharma <rahulsharma94595@gmail.com>
2020-09-03 00:35:49 +0530

    (Change) i%5===0 Buzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -3,7 +3,7 @@ function FizzBuzz(num) {
     for(let i=1; i<= num; i++) {
         if(i%15 === 0) console.log('FizzBuzz');
         else if(i%3 === 0) console.log('Fizz');
-        else if(i%5 === 0 )console.log('FizzBUzz');
+        else if(i%5 === 0 )console.log('Buzz');
         else console.log(i);
     }
 }
